94baa1d62798d7ae8c274a896e223d66628a6791
prayagsingh <prayagsingh03@gmail.com>
2021-08-21 20:05:27 +0530

    template cache

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -11,7 +11,7 @@ func Home(rw http.ResponseWriter, r *http.Request) {
 	render.RenderTemplate(rw, "home.page.html")
 }
 
-// About is the about page handler
+// About is "about Us" page handler
 func About(rw http.ResponseWriter, r *http.Request) {
 	render.RenderTemplate(rw, "about.page.html")
 }
--- pkg/render/renderTemplate.go
+++ pkg/render/renderTemplate.go
@@ -1,8 +1,10 @@
 package render
 
 import (
+	"bytes"
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
 	"path/filepath"
 )
@@ -12,60 +14,82 @@ var functions = template.FuncMap{}
 // RenderTemplate for rendering the template using html/template
 func RenderTemplate(rw http.ResponseWriter, tmpl string) {
 
-	_, err := RenderTemplateTest(rw)
+	templateCache, err := CreateTemplateCache()
 	if err != nil {
-		fmt.Println("Error getting template Cache", err)
+		log.Fatal(err)
 		return
 	}
 
-	parseTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-	err = parseTemplate.Execute(rw, nil)
+	// ok is used to check if the template exists or not
+	// if template found, ok wil return true else false
+	t, ok := templateCache[tmpl]
+
+	fmt.Println("t is ", t.DefinedTemplates())
+	fmt.Println("Value of oK is ", ok)
+	if !ok {
+		log.Fatal("Could not get template from template cache")
+	}
+
+	buf := new(bytes.Buffer)
+	err = t.Execute(buf, nil)
 	if err != nil {
-		fmt.Println("Error parsing template", err)
+		fmt.Println("Error writing parsed template to buffer", err)
 		return
 	}
+	n, err := buf.WriteTo(rw)
+
+	if err != nil {
+		fmt.Println("Error writing template to browser", err)
+		return
+	}
+
+	fmt.Println("Value of n is ", n)
 }
 
-// RenderTemplateTest for template caching
-func RenderTemplateTest(rw http.ResponseWriter) (map[string]*template.Template, error) {
+// CreateTemplateCache creates a template cache as a map
+func CreateTemplateCache() (map[string]*template.Template, error) {
 
 	// store the template found during parsing
-	mycache := map[string]*template.Template{}
+	myCache := map[string]*template.Template{}
 
 	// find all the pages in template dir which ends with page.html
 	pages, err := filepath.Glob("./templates/*.page.html")
 	if err != nil {
-		return mycache, err
+		return myCache, err
 	}
 
+	fmt.Println("pages are:", pages)
 	for _, page := range pages {
 		name := filepath.Base(page)
-		fmt.Println("Page is currently ", page)
+		fmt.Printf("Page is currently %s and name is %s \n", page, name)
 		// New allocates the new template with a given name
 		// Funcs adds the elements of the argument map to the template's function map. It must be
 		// 	 called before the template is parsed.
 		// ParseFiles parses the named files and associates the resulting templates with t. If an
-		//   error occurs, parsing stops and the returned template is nil; otherwise it is t
+		//   error occurs, parsing stops and the returned template is nil otherwise it is t
 		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
 		if err != nil {
-			return mycache, err
+			return myCache, err
 		}
 
 		matches, err := filepath.Glob("./templates/*.layout.html")
 		if err != nil {
-			return mycache, err
+			return myCache, err
 		}
-
+		fmt.Println("matches is ", matches)
 		if len(matches) > 0 {
+			//fmt.Println("length of matches is ", len(matches))
 			ts, err = template.ParseGlob("./templates/*.layout.html")
 			if err != nil {
-				return mycache, err
+				return myCache, err
 			}
 		}
 
 		// Adding templates to cache
-		mycache[name] = ts
+		myCache[name] = ts
+		fmt.Println("Defined templates stored in myCache is ", myCache[name].DefinedTemplates())
+		fmt.Println("template stored in myCache is ", myCache[name].Tree.Name)
 	}
 
-	return mycache, nil
+	return myCache, nil
 }
